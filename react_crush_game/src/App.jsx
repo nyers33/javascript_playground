166434e42d3fc6e0ee9a54601eebbc2b388f3f25
Tibor Nyers <tibornyers@gmail.com>
2025-01-16 17:11:47 +0100

    cell selection

--- react_crush_game/src/App.jsx
+++ react_crush_game/src/App.jsx
@@ -10,28 +10,72 @@ let App = function CrushGame() {
 	const nCellYIdeal = 6;
 
   const [bIsInitialized, setIsInitialized] = useState(0);
+  const [bCellsWereSwapped, setCellsWereSwapped] = useState(0);
 	const [nItem, setNumberOfItem] = useState(nItemIdeal);
 	const [nCellX, setNumberOfCellX] = useState(nCellXIdeal);
 	const [nCellY, setNumberOfCellY] = useState(nCellYIdeal);
-	const [cellValues, setCellValues] = useState(Array(nCellX * nCellY).fill(0));
+	const [cellValues, setCellValues] = useState(Array(nCellX * nCellY).fill(null));
+  const [cellToSwap1st, setCellToSwap1st] = useState(null);
+  const [cellToSwap2nd, setCellToSwap2nd] = useState(null);
+
+  function onCellMouseDown(cellDown) {
+    setCellsWereSwapped(0);
+    setCellToSwap1st(cellDown);
+    setCellToSwap2nd(null);
+  }
+
+  function onCellMouseUp(cellUp) {
+    if (cellToSwap1st !== null && cellToSwap1st != cellUp) {
+
+      const bIsFirstRow = cellToSwap1st < nCellX;
+      const bIsLastRow = cellToSwap1st >= nCellX * nCellY - nCellX;
+      const bIsFirstCol = cellToSwap1st % nCellX === 0;
+      const bIsLastCol = (cellToSwap1st + 1) % nCellX === 0;
+
+      const offsetCells = [
+        bIsFirstRow ? cellToSwap1st : cellToSwap1st - nCellX,
+        bIsLastRow ? cellToSwap1st : cellToSwap1st + nCellX,
+        bIsFirstCol ? cellToSwap1st : cellToSwap1st - 1,
+        bIsLastCol ? cellToSwap1st : cellToSwap1st + 1
+      ];
+
+      if (offsetCells.includes(cellUp)) {
+        // do the swap
+        if (cellToSwap1st !== null && cellUp !== null) {
+          [cellValues[cellToSwap1st], cellValues[cellUp]] = [cellValues[cellUp], cellValues[cellToSwap1st]];
+          setCellsWereSwapped(1);
+          setCellToSwap2nd(cellUp);
+        }
+      } else {
+        setCellToSwap1st(null);
+        setCellToSwap2nd(null);
+      }
+    }
+  }
 
   function destroyGame() {
     setIsInitialized(0);
-    setCellValues(Array(nCellX * nCellY).fill(0));
+    setCellsWereSwapped(0);
+    setCellValues(Array(nCellX * nCellY).fill(null));
+    setCellToSwap1st(null);
+    setCellToSwap2nd(null);
   }
   
   function initGame() {
     setIsInitialized(1);
+    setCellsWereSwapped(0);
     setCellValues(
       Array.from(
         { length: nCellX * nCellY }, () => Math.floor(Math.random() * nItem + 1)
       )
     );
+    setCellToSwap1st(null);
+    setCellToSwap2nd(null);
   }
   
   function tweakCounter(setCount, currentCount, tweakValue = 0, counterLimitMin = 0, counterLimitMax = 99) {
     setCount(Math.min(Math.max(currentCount + tweakValue, counterLimitMin), counterLimitMax));
-  }
+ }
   
   function TweakValueButton({ onClick, text, disabled }) {
     return (
@@ -45,7 +89,18 @@ let App = function CrushGame() {
         {Array.from({ length: xDim }, (_, cellIndex) => {
           const cellIndexGlobal = rowIndex * xDim + cellIndex;
           const colorClass = `item${cellValues[cellIndexGlobal]}`;
-          return <td key={cellIndex} className={colorClass}></td>;
+          const isSelected1st = cellToSwap1st === cellIndexGlobal; 
+          const isSelected2nd = cellToSwap2nd === cellIndexGlobal; 
+
+          return <td
+            key={cellIndex}
+            className={colorClass}
+            onMouseDown={() => onCellMouseDown(cellIndexGlobal)}
+            onMouseUp={() => onCellMouseUp(cellIndexGlobal)}
+            style={{
+              border: isSelected1st || isSelected2nd ? "2px solid white" : "2px solid black",
+          }}>
+          </td>;
         })}
       </tr>
     ));
